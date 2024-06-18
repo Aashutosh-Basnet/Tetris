d840235165988e59e810d300df21e22f2bae39af
Aashutosh Basnet <aashutoshbasnet3344@gmail.com>
2024-06-18 21:45:50 +0530

    update css

---
+++ tetris/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="Apps">
+      <h1>Hello, it's me Aashutosh. I am CS student. Aren't I? Hello.</h1>
+    </div>
+  );
+}
+
+export default App;
